059391d40d88e78932d51719abadd1b7fa1276e2
DimonBor <firecat@orangefox.tech>
2022-10-20 11:22:08 +0300

    SZK Lab_2 update

--- SZK/Lab_2/CaesarChipher/__main__.py
+++ SZK/Lab_2/CaesarChipher/__main__.py
@@ -1,8 +1,8 @@
 import sys
 
 alphabet = [
-    'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 
-    'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 
+    'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й',
+    'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч',
     'ш', 'щ', 'ь', 'ю', 'я', ' '
 ]
 
@@ -10,7 +10,7 @@ alphabet = [
 def caesar(string, key):
     output = ""
     for i in string:
-        index = (alphabet.index(i)+key)%len(alphabet)
+        index = (alphabet.index(i)+key) % len(alphabet)
         output += alphabet[index]
 
     return output
@@ -31,19 +31,20 @@ def frequencyAnalysis(string):
     for letter in string: 
         try: lettersFreq[letter] += 1
         except: lettersFreq[letter] = 1
-    possibleSpace = max(lettersFreq, key=lettersFreq.get) 
-    key = alphabet.index(possibleSpace)+1  
+    print(lettersFreq)
+    possibleSpace = max(lettersFreq, key=lettersFreq.get)
+    key = alphabet.index(possibleSpace)+1
     print(
-        f"Result with key {key}:\n\n",
+        f"Result with key {key}:\n",
         uncaesar(string, key),
         "\n"
     )
 
 
-def main(args=sys.argv): 
+def main(args=sys.argv):
     # Using file as input source
     if "--file" in args:
-        try: 
+        try:
             fileName = args[args.index("--file")+1]
         except:
             print("ERROR, no file specified!")
@@ -55,11 +56,11 @@ def main(args=sys.argv):
             print("ERROR, can`t open file!")
             return 0
         
-    # Using plain text as input source 
+    # Using plain text as input source
     else:
         inputText = input("Enter input text: ")
 
-    # Taking key from args 
+    # Taking key from args
     try: 
         key = int(args[args.index("--key")+1])
     except:
@@ -68,7 +69,9 @@ def main(args=sys.argv):
             return 0
         else: pass
 
-    # Choosing an option 
+    print(f"Input text:\n {inputText}\n")
+
+    # Choosing an option
     if "--encrypt" in args:
         print(f"Encrypted text is:\n{caesar(inputText, key)}")
     if "--decrypt" in args:
