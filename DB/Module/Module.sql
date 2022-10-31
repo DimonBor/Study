ad4ec69c8199d5c6dcfb44f5f3cc0d27317bbc63
DimonBor <firecat@orangefox.tech>
2022-10-31 11:27:37 +0200

    DB Module Update

--- DB/Module/Module.sql
+++ DB/Module/Module.sql
@@ -1,3 +1,5 @@
+DROP TABLE StudInCourse;
+DROP TABLE Authors;
 DROP TABLE Grades;
 DROP TABLE Reports;
 DROP TABLE Tasks;
@@ -11,9 +13,6 @@ DROP SEQUENCE Material_seq;
 DROP SEQUENCE Reports_seq;
 DROP SEQUENCE Grades_seq;
 DROP SEQUENCE Tasks_seq;
-
-CREATE OR REPLACE TYPE people_array IS VARRAY (30) OF NUMBER;
-/
 
 CREATE TABLE People (
   id number(10) PRIMARY KEY,
@@ -35,8 +34,13 @@ CREATE TABLE Disciplines (
   id number(10) PRIMARY KEY,
   name varchar(100) NOT NULL,
   teacher number(10) NOT NULL,
-  assistant number(10),
-  students people_array NOT NULL
+  assistant number(10)
+);
+
+CREATE TABLE StudInCourse (
+  studID number(10) NOT NULL,
+  disciplineID number(10) NOT NULL,
+  CONSTRAINT stud_key_pair PRIMARY KEY (studID, disciplineID)
 );
 
 CREATE SEQUENCE Disciplines_seq START WITH 1 INCREMENT BY 1;
@@ -55,7 +59,7 @@ CREATE TABLE Tasks (
   discipline number(10) NOT NULL,
   author number(10) NOT NULL,
   max_grade number(10),
-  material varchar(255) NOT NULL
+  material number(10)
 );
 
 CREATE SEQUENCE Tasks_seq START WITH 1 INCREMENT BY 1;
@@ -70,9 +74,14 @@ END;
 
 CREATE TABLE Material (
   id number(10) PRIMARY KEY,
-  authors people_array NOT NULL,
   discipline number(10) NOT NULL,
   material varchar(255) NOT NULL
+);
+
+CREATE TABLE Authors (
+  authID number(10) NOT NULL,
+  materialID number(10) NOT NULL,
+  CONSTRAINT authors_key_pair PRIMARY KEY (authID, materialID)
 );
 
 CREATE SEQUENCE Material_seq START WITH 1 INCREMENT BY 1;
@@ -135,26 +144,41 @@ ALTER TABLE Material ADD FOREIGN KEY (discipline) REFERENCES Disciplines (id);
 ALTER TABLE Reports ADD FOREIGN KEY (discipline) REFERENCES Disciplines (id);
 ALTER TABLE Grades ADD FOREIGN KEY (discipline) REFERENCES Disciplines (id);
 ALTER TABLE Grades ADD FOREIGN KEY (report) REFERENCES Reports (id);
+ALTER TABLE StudInCourse ADD FOREIGN KEY (studID) REFERENCES People (id);
+ALTER TABLE StudInCourse ADD FOREIGN KEY (disciplineID) REFERENCES Disciplines (id);
+ALTER TABLE Authors ADD FOREIGN KEY (authID) REFERENCES People (id);
+ALTER TABLE Authors ADD FOREIGN KEY (materialID) REFERENCES Material (id);
+ALTER TABLE Tasks ADD FOREIGN KEY (material) REFERENCES Material (id);
 
 INSERT INTO People (name, status) VALUES ('Borys', 'teacher');
 INSERT INTO People (name, status) VALUES ('Dmytro', 'student');
 INSERT INTO People (name, status) VALUES ('Petro', 'student');
 INSERT INTO People (name, status) VALUES ('Danil', 'student');
 
-INSERT INTO Disciplines (name, teacher, students) VALUES ('Data Bases', 1, people_array(2, 3, 4));
+INSERT INTO Disciplines (name, teacher) VALUES ('Data Bases', 1);
 
-INSERT INTO Material (authors, discipline, material) VALUES (people_array(1), 1, 'material_url');
-INSERT INTO Material (authors, discipline, material) VALUES (people_array(1), 1, 'material_url');
+INSERT INTO StudInCourse (studID, disciplineID) VALUES (2, 1);
+INSERT INTO StudInCourse (studID, disciplineID) VALUES (3, 1);
+INSERT INTO StudInCourse (studID, disciplineID) VALUES (4, 1);
 
-INSERT INTO Tasks (name, discipline, author, max_grade, material) VALUES ('First task', 1, 1, 100, 'material_url1');
-INSERT INTO Tasks (name, discipline, author, max_grade, material) VALUES ('Second task', 1, 1, 100, 'material_url2'); 
+INSERT INTO Material (discipline, material) VALUES (1, 'material_url');
+INSERT INTO Material (discipline, material) VALUES (1, 'material_url');
+
+INSERT INTO Authors (authID, materialID) VALUES(1, 1); 
+INSERT INTO Authors (authID, materialID) VALUES(1, 2);
+
+INSERT INTO Tasks (name, discipline, author, max_grade, material) VALUES ('First task', 1, 1, 100, 1);
+INSERT INTO Tasks (name, discipline, author, max_grade, material) VALUES ('Second task', 1, 1, 100, 2); 
+INSERT INTO Tasks (name, discipline, author, max_grade) VALUES ('Third task', 1, 1, 100); 
 
 INSERT INTO Reports (discipline, task, report_date, student, material) VALUES (1, 1, to_date('10-OCT-2022 17:04', 'DD-MON-YYYY HH24:MI'), 2, 'report_url1');
 INSERT INTO Reports (discipline, task, report_date, student, material) VALUES (1, 2, to_date('11-OCT-2022 04:20', 'DD-MON-YYYY HH24:MI'), 2, 'report_url2');
 INSERT INTO Reports (discipline, task, report_date, student, material) VALUES (1, 1, to_date('13-OCT-2022 20:20', 'DD-MON-YYYY HH24:MI'), 3, 'report_url3');
 INSERT INTO Reports (discipline, task, report_date, student, material) VALUES (1, 1, to_date('10-OCT-2022 07:45', 'DD-MON-YYYY HH24:MI'), 4, 'report_url4');
 INSERT INTO Reports (discipline, task, report_date, student, material) VALUES (1, 2, to_date('11-OCT-2022 15:01', 'DD-MON-YYYY HH24:MI'), 4, 'report_url5');
-
+INSERT INTO Reports (discipline, task, report_date, student, material) VALUES (1, 2, to_date('15-OCT-2022 15:01', 'DD-MON-YYYY HH24:MI'), 4, 'report_url6');
+INSERT INTO Reports (discipline, task, report_date, student, material) VALUES (1, 2, to_date('15-OCT-2022 16:05', 'DD-MON-YYYY HH24:MI'), 4, 'report_url7');
+ 
 INSERT INTO Grades (student, teacher, discipline, report, grade) VALUES (2, 1, 1, 1, 90);
 INSERT INTO Grades (student, teacher, discipline, report, grade) VALUES (2, 1, 1, 2, 82);
 INSERT INTO Grades (student, teacher, discipline, report, grade) VALUES (3, 1, 1, 3, 55);
@@ -164,4 +188,3 @@ INSERT INTO Grades (student, teacher, discipline, report, grade) VALUES (4, 1, 1
 COLUMN NAME FORMAT A50;
 COLUMN MATERIAL FORMAT A50;
 COLUMN STATUS FORMAT A50;
-COLUMN STUDENTS FORMAT A50;
