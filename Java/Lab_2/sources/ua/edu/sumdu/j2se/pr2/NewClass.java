56cf85eb03dd84a0bfb4904795596979a4ffb7c5
DimonBor <firecat@orangefox.tech>
2023-02-21 12:44:21 +0200

    Java | Lab 2 update

--- Java/Lab_2/sources/ua/edu/sumdu/j2se/pr2/NewClass.java
+++ Java/Lab_2/sources/ua/edu/sumdu/j2se/pr2/NewClass.java
@@ -6,31 +6,40 @@ public class NewClass {
 
 	public NewClass () {
 		Scanner k = new Scanner(System.in);
+        
+        int number1;	
+		float number2;
+		double number3;
+		char char1;	
+		byte number4;	
+		short number5;	
+		String word;
+		long number6;
 
 		System.out.println ("Введіть довільне ціле число: "); // int 
-		int number1 = k.nextInt();	
+		number1 = k.nextInt();	
 
         System.out.println ("Введіть довільне неціле число: "); // float 
-		float number2 = k.nextFloat();	
+		number2 = k.nextFloat();	
 
         System.out.println ("Ще одне: "); // double
-		double number3 = k.nextFloat();	
+		number3 = k.nextDouble();	
 
         System.out.println ("Введіть довільний символ: "); // char
-		char char1 = k.next().charAt(0);	
+		char1 = k.next().charAt(0);	
 	
         System.out.println ("Введіть цифру від 1 до 9: "); // byte 
-		byte number4 = k.nextByte();	
+		number4 = k.nextByte();	
 	
         System.out.println ("Введіть число від 1 до 999: "); // short
-		short number5 = k.nextShort();	
+		number5 = k.nextShort();	
         k.nextLine();
         
         System.out.println ("Введіть довільне слово: "); // String 
-		String word = k.nextLine();	
+		word = k.nextLine();	
 	
         System.out.println ("І ще одне ціле число: "); // long
-		long number6 = k.nextLong();	
+		number6 = k.nextLong();	
 
         System.out.println(
             "\nВи понавводили:\n" +
